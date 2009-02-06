a594f5c598cee679f7c040510483d945d7a8f2ae
José Valim <jose.valim@gmail.com>
2009-02-06 17:27:45 +0100

    Eager loading plugin to work in multithreaded environments (but not releasing as gem since I want to check it more!)

--- lib/inherited_resources.rb
+++ lib/inherited_resources.rb
@@ -1,4 +1,19 @@
 dir = File.dirname(__FILE__)
 require File.join(dir, 'inherited_resources', 'respond_to')
 
+unless defined?(ApplicationController)
+  class ApplicationController < ActionController::Base; end
+end
+
 module InheritedResources; end
+
+# Load InheritedResources::Base after defining ApplicationController
+require File.dirname(__FILE__) + '/../lib/inherited_resources/base_helpers.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/belongs_to.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/belongs_to_helpers.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/class_methods.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/dumb_responder.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/polymorphic_helpers.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/singleton_helpers.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/url_helpers.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources/base.rb'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,21 +20,7 @@ I18n.load_path << File.join(File.dirname(__FILE__), 'fixtures', 'en.yml')
 I18n.reload!
 
 # Load respond_to before defining ApplicationController
-require File.dirname(__FILE__) + '/../lib/inherited_resources/respond_to.rb'
-
-# Define ApplicationController
-class ApplicationController < ActionController::Base; end
-
-# Load InheritedResources::Base after defining ApplicationController
-require File.dirname(__FILE__) + '/../lib/inherited_resources/base_helpers.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/belongs_to.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/belongs_to_helpers.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/class_methods.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/dumb_responder.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/polymorphic_helpers.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/singleton_helpers.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/url_helpers.rb'
-require File.dirname(__FILE__) + '/../lib/inherited_resources/base.rb'
+require File.dirname(__FILE__) + '/../lib/inherited_resources.rb'
 
 # Define view_paths
 ActionController::Base.view_paths = File.join(File.dirname(__FILE__), 'views')
