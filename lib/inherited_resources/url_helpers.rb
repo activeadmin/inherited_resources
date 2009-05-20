4108d80a078855a5c734c82b91741a14e44ec69b
José Valim <jose.valim@gmail.com>
2009-05-20 15:54:12 +0200

    Huge refactoring on url helpers. Much cleaner.

--- lib/inherited_resources/url_helpers.rb
+++ lib/inherited_resources/url_helpers.rb
@@ -42,7 +42,6 @@ module InheritedResources
     def self.create_resources_url_helpers!(base)
       resource_segments, resource_ivars = [], []
       resource_config = base.resources_configuration[:self]
-      polymorphic = false
 
       # Add route_prefix if any.
       resource_segments << resource_config[:route_prefix] unless resource_config[:route_prefix].blank?
@@ -53,148 +52,106 @@ module InheritedResources
       #
       base.parents_symbols.map do |symbol|
         if symbol == :polymorphic
-          polymorphic = true
           resource_ivars << :parent
         else
           config = base.resources_configuration[symbol]
           resource_segments << config[:route_name]
-          resource_ivars    << config[:instance_name]
+          resource_ivars    << :"@#{config[:instance_name]}"
         end
       end
 
-      # Deals with singleton.
-      #
-      # If not a singleton, we add the current collection name and build the
-      # collection url. It can build for example:
-      #
-      #   project_tasks_url
-      #
-      # If it's a singleton we also build a collection, just for compatibility.
-      # The collection_url for singleton is the parent show url. For example,
-      # if we have ProjectsController with ManagerController, where the second
-      # is the singleton, the collection url would be: project_url(@project).
-      #
-      # This is where you are going to be redirected after destroying the manager.
-      #
-      unless base.resources_configuration[:self][:singleton]
-        resource_segments << resource_config[:route_collection_name]
-        generate_url_and_path_helpers(base, nil, :collection, resource_segments, resource_ivars, polymorphic)
-        resource_segments.pop
-      else
-        generate_url_and_path_helpers(base, nil, :collection, resource_segments, resource_ivars, polymorphic)
-      end
+      singleton   = base.resources_configuration[:self][:singleton]
+      polymorphic = base.parents_symbols.include?(:polymorphic)
 
-      # Prepare and add new_resource_url
-      resource_segments << resource_config[:route_instance_name]
-      generate_url_and_path_helpers(base, :new, :resource, resource_segments, resource_ivars, polymorphic)
+      collection_ivars    = resource_ivars.dup
+      collection_segments = resource_segments.dup
 
-      # We don't add the resource_ivar to edit and show url if singleton.
-      # Singletons are simply:
+      # This is the default route configuration, later we have to deal with
+      # exception from polymorphic and singleton cases.
       #
-      #   edit_project_manager_url(@project)
+      collection_segments << resource_config[:route_collection_name]
+      resource_segments   << resource_config[:route_instance_name]
+      resource_ivars      << :"@#{resource_config[:instance_name]}"
+
+      # In singleton cases, we do not send the current element instance variable
+      # because the id is not in the URL. For example, we should call:
+      #
+      #   project_manager_url(@project)
       #
       # Instead of:
       #
-      #   edit_project_manager_url(@project, @manager)
+      #   project_manager_url(@project, @manager)
       #
-      resource_ivars << resource_config[:instance_name] unless base.resources_configuration[:self][:singleton]
+      # Another exception in singleton cases is that collection url does not
+      # exist. In such cases, we create the parent collection url. So in the
+      # manager case above, the collection url will be:
+      #
+      #    project_url(@project)
+      #
+      # If the singleton does not have a parent, it will default to root_url.
+      #
+      # Finally, polymorphic cases we have to give hints to the polymorphic url
+      # builder. This works by attaching new ivars as symbols or records.
+      #
+      if singleton
+        collection_segments.pop
+        resource_ivars.pop
 
-      # Prepare and add resource_url and edit_resource_url
-      generate_url_and_path_helpers(base, nil, :resource, resource_segments, resource_ivars, polymorphic)
-      generate_url_and_path_helpers(base, :edit, :resource, resource_segments, resource_ivars, polymorphic)
+        if polymorphic
+          resource_ivars << resource_config[:instance_name].inspect
+          new_ivars       = resource_ivars
+        end
+      elsif polymorphic
+        collection_ivars << 'resource_class.new'
+      end
+
+      generate_url_and_path_helpers base, nil,   :collection, collection_segments, collection_ivars
+      generate_url_and_path_helpers base, :new,  :resource,   resource_segments,   new_ivars || collection_ivars
+      generate_url_and_path_helpers base, nil,   :resource,   resource_segments,   resource_ivars
+      generate_url_and_path_helpers base, :edit, :resource,   resource_segments,   resource_ivars
     end
 
-    def self.generate_url_and_path_helpers(base, prefix, name, resource_segments, resource_ivars, polymorphic=false) #:nodoc:
-        ivars = resource_ivars.map{|i| i == :parent ? :parent : "@#{i}" }
+    def self.generate_url_and_path_helpers(base, prefix, name, resource_segments, resource_ivars) #:nodoc:
+      ivars = resource_ivars.dup
 
-        # If it's not a singleton, ivars are not empty, not a collection or
-        # not a "new" named route, we can pass a resource as argument.
-        #
-        unless base.resources_configuration[:self][:singleton] || ivars.empty? ||
-               name == :collection || prefix == :new
-          ivars.push "(given_args.first || #{ivars.pop})"
-        end
+      singleton   = base.resources_configuration[:self][:singleton]
+      polymorphic = base.parents_symbols.include?(:polymorphic)
 
-        # When polymorphic is true, the segments must be replace by :polymorphic
-        # and ivars should be gathered into an array.
-        #
-        if polymorphic
-          segments = :polymorphic
+      # If it's not a singleton, ivars are not empty, not a collection or
+      # not a "new" named route, we can pass a resource as argument.
+      #
+      unless singleton || ivars.empty? || name == :collection || prefix == :new
+        ivars.push "(given_args.first || #{ivars.pop})"
+      end
 
-          # Customization to allow polymorphic with singletons.
-          #
-          # Let's take the projects and companies where each one has one manager
-          # example. The url helpers would be:
-          #
-          #   company_manager_url(@company)
-          #   project_manager_url(@project)
-          #
-          # Notice how the manager is not sent in the helper, because it's a
-          # singleton. So, polymorphic urls would be:
-          #
-          #   polymorphic_url(@company)
-          #   polymorphic_url(@project)
-          #
-          # Obviously, this won't work properly. So in such polymorphic with
-          # singletons cases we have to do this:
-          #
-          #   polymorphic_url(@company, 'manager')
-          #   polymorphic_url(@project, 'manager')
-          #
-          # This is exactly what we are doing here.
-          #
-          # The other case to handle is collection and new helpers with
-          # polymorphic urls. In such cases, we usually would not send anything:
-          #
-          #   project_tasks_url(@project)
-          #   new_project_task_url(@project)
-          #
-          # But this wouldn't work with polymorphic urls by the same reason as
-          # singletons:
-          #
-          #   polymorphic_url(@project)
-          #   new_polymorphic_url(@project)
-          #
-          # So we have to do this:
-          #
-          #   polymorphic_url(@project, Task.new)
-          #   new_polymorphic_url(@project, Task.new)
-          #
-          if base.resources_configuration[:self][:singleton]
-            ivars << base.resources_configuration[:self][:instance_name].inspect unless name == :collection
-          elsif name == :collection || prefix == :new
-            ivars << 'resource_class.new'
+      # When polymorphic is true, the segments must be replace by :polymorphic
+      # and ivars should be gathered into an array, which is compacted when
+      # optional.
+      #
+      if polymorphic
+        segments = :polymorphic
+        ivars    = "[#{ivars.join(', ')}]"
+        ivars   << '.compact' if base.resources_configuration[:polymorphic][:optional]
+      else
+        segments = resource_segments.empty? ? 'root' : resource_segments.join('_')
+        ivars    = ivars.join(', ')
+      end
+
+      prefix = prefix ? "#{prefix}_" : ''
+      ivars << (ivars.empty? ? 'given_options' : ', given_options')
+
+      base.class_eval <<-URL_HELPERS, __FILE__, __LINE__
+        protected
+          def #{prefix}#{name}_path(*given_args)
+            given_options = given_args.extract_options!
+            #{prefix}#{segments}_path(#{ivars})
           end
 
-          ivars  = "[#{ivars.join(', ')}]"
-
-          # Add compact to deal with polymorphic optional associations.
-          ivars << '.compact' if base.resources_configuration[:polymorphic][:optional]
-        else
-          # In the last case, if segments is empty (this usually happens with
-          # root singleton resources, we set it to root)
-          #
-          segments = resource_segments.empty? ? 'root' : resource_segments.join('_')
-          ivars    = ivars.join(', ')
-        end
-
-        prefix = prefix ? "#{prefix}_" : ''
-
-        # Add given_options to ivars
-        ivars << (ivars.empty? ? 'given_options' : ', given_options')
-
-        base.class_eval <<URL_HELPERS, __FILE__, __LINE__
-protected
-  def #{prefix}#{name}_path(*given_args)
-    given_options = given_args.extract_options!
-    #{prefix}#{segments}_path(#{ivars})
-  end
-
-  def #{prefix}#{name}_url(*given_args)
-    given_options = given_args.extract_options!
-    #{prefix}#{segments}_url(#{ivars})
-  end
-URL_HELPERS
+          def #{prefix}#{name}_url(*given_args)
+            given_options = given_args.extract_options!
+            #{prefix}#{segments}_url(#{ivars})
+          end
+      URL_HELPERS
     end
 
   end
