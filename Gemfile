6e3dd36116124880111e4eee727e771a56e1049e
José Valim <jose.valim@gmail.com>
2010-06-26 15:41:02 +0200

    Use bundler.

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "http://rubygems.org"
+
+gem "rails", :path => "../rails"
+gem "responders", "~> 0.6.0"
+gem "has_scope", "~> 0.5.0"
+gem "mocha"
+
+if RUBY_VERSION < "1.9"
+	gem "ruby-debug"
+else
+  gem "test-unit"
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,7 @@
 require 'rubygems'
+require 'bundler'
 
-gem 'responders', '0.6.0'
-gem 'activesupport', '3.0.0.beta2'
-gem 'activemodel', '3.0.0.beta2'
-gem 'actionpack', '3.0.0.beta2'
-gem 'railties', '3.0.0.beta2'
-
-begin
-  gem "test-unit"
-rescue LoadError
-end
-
-begin
-  gem "ruby-debug"
-  require 'ruby-debug'
-rescue LoadError
-end
+Bundler.setup
 
 require 'test/unit'
 require 'mocha'
@@ -25,10 +11,7 @@ RAILS_ROOT = "anywhere"
 
 require "active_support"
 require "active_model"
-
 require "action_controller"
-require "action_dispatch/middleware/flash"
-
 require "rails/railtie"
 
 I18n.load_path << File.join(File.dirname(__FILE__), 'locales', 'en.yml')
@@ -37,8 +20,8 @@ I18n.reload!
 class ApplicationController < ActionController::Base; end
 
 # Add IR to load path and load the main file
-ActiveSupport::Dependencies.load_paths << File.expand_path(File.dirname(__FILE__) + '/../lib')
-require_dependency 'inherited_resources'
+$:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
+require 'inherited_resources'
 
 ActionController::Base.view_paths = File.join(File.dirname(__FILE__), 'views')
 
