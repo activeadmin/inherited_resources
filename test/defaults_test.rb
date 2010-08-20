0c240751e1ff69442d5d4bfbae5b193931349815
James F. Herdman <james.herdman@me.com>
2010-08-21 04:57:46 +0800

    Fix path to test_helper.rb

--- test/defaults_test.rb
+++ test/defaults_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require File.expand_path('test_helper', File.dirname(__FILE__))
 
 class Malarz
   def self.human_name; 'Painter'; end
--- test/singleton_test.rb
+++ test/singleton_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require File.expand_path('test_helper', File.dirname(__FILE__))
 
 # This test file is instead to test the how controller flow and actions
 # using a belongs_to association. This is done using mocks a la rspec.
