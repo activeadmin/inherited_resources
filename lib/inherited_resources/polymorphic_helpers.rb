e95449165ac0b4f90889ee7e301cc61612481245
Karst Hammer <karst@noxa.nl>
2010-10-15 16:49:59 +0200

    Added support for multiple polymorphic optional nesting.
    Added tests for multiple polymorphic optional nesting.

--- lib/inherited_resources/polymorphic_helpers.rb
+++ lib/inherited_resources/polymorphic_helpers.rb
@@ -126,28 +126,28 @@ module InheritedResources
       #
       def symbols_for_association_chain #:nodoc:
         polymorphic_config = resources_configuration[:polymorphic]
-
         parents_symbols.map do |symbol|
           if symbol == :polymorphic
             params_keys = params.keys
 
-            key = polymorphic_config[:symbols].find do |poly|
-              params_keys.include? resources_configuration[poly][:param].to_s
-            end
+            keys = polymorphic_config[:symbols].map do |poly|
+              params_keys.include?(resources_configuration[poly][:param].to_s) ? poly : nil
+            end.compact
 
-            if key.nil?
+            if keys.empty?
               raise ScriptError, "Could not find param for polymorphic association. The request" <<
                                  "parameters are #{params.keys.inspect} and the polymorphic " <<
                                  "associations are #{polymorphic_config[:symbols].inspect}." unless polymorphic_config[:optional]
 
               nil
             else
-              @parent_type = key.to_sym
+              @parent_type = keys[-1].to_sym
+							@parent_types = keys.map(&:to_sym)
             end
           else
             symbol
           end
-        end.compact
+        end.flatten.compact
       end
 
   end
