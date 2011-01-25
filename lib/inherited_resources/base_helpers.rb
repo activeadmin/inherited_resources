7fb9e76e5f48fdc8f507c5165881db66c0d10c14
Adam Wróbel <adam@fluxinc.ca>
2011-01-26 07:16:47 +0800

    Use scoped() instead of all() for collection.

--- lib/inherited_resources/base_helpers.rb
+++ lib/inherited_resources/base_helpers.rb
@@ -20,7 +20,7 @@ module InheritedResources
       #   end
       #
       def collection
-        get_collection_ivar || set_collection_ivar(end_of_association_chain.all)
+        get_collection_ivar || set_collection_ivar(end_of_association_chain.scoped)
       end
 
       # This is how the resource is loaded.
