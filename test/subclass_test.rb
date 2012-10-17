2408f3e388c0fe0c73112d5522470d1bb64c1fdb
Paul Gallagher <gallagher.paul@gmail.com>
2012-10-17 17:43:55 +0800

    ensure subclassed controllers can override default resource_class

--- test/subclass_test.rb
+++ test/subclass_test.rb
@@ -17,3 +17,18 @@ class SubclassedResourceController < ActionController::TestCase
     assert_equal Livarot, @controller.resource_class
   end
 end
+
+class Roquefort
+end
+class RoquesController < CheesesController
+  defaults :resource_class => Roquefort
+end
+class SubclassedResourceWithCustomNameController < ActionController::TestCase
+  tests RoquesController
+
+  def test_that_it_picked_the_subclass_model
+    # make public so we can test it
+    RoquesController.send(:public, *RoquesController.protected_instance_methods)
+    assert_equal Roquefort, @controller.resource_class
+  end
+end
