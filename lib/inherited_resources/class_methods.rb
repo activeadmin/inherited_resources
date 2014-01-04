3f89438719c676ce22ddd9e262bb6462d568a129
Aaron Stone <aaron@serendipity.cx>
2014-01-04 08:12:52 -0800

    Revert "Unsplatting simplified"
    This reverts commit bf659843dccec5b72b105e6ff9a581a84b9d53ef.

--- lib/inherited_resources/class_methods.rb
+++ lib/inherited_resources/class_methods.rb
@@ -222,7 +222,7 @@ module InheritedResources
       def polymorphic_belongs_to(*symbols, &block)
         options = symbols.extract_options!
         options.merge!(:polymorphic => true)
-        belongs_to(*symbols, options, &block)
+        belongs_to(*symbols << options, &block)
       end
 
       # A quick method to declare singleton belongs to.
@@ -230,7 +230,7 @@ module InheritedResources
       def singleton_belongs_to(*symbols, &block)
         options = symbols.extract_options!
         options.merge!(:singleton => true)
-        belongs_to(*symbols, options, &block)
+        belongs_to(*symbols << options, &block)
       end
 
       # A quick method to declare optional belongs to.
@@ -238,7 +238,7 @@ module InheritedResources
       def optional_belongs_to(*symbols, &block)
         options = symbols.extract_options!
         options.merge!(:optional => true)
-        belongs_to(*symbols, options, &block)
+        belongs_to(*symbols << options, &block)
       end
 
       # Defines custom restful actions by resource or collection basis.
@@ -379,7 +379,7 @@ module InheritedResources
       def create_custom_action(resource_or_collection, action)
         class_eval <<-CUSTOM_ACTION, __FILE__, __LINE__
           def #{action}(options={}, &block)
-            respond_with(*with_chain(#{resource_or_collection}), options, &block)
+            respond_with(*(with_chain(#{resource_or_collection}) << options), &block)
           end
           alias :#{action}! :#{action}
           protected :#{action}!
