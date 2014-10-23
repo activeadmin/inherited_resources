7be1cbf63f64794ce978ca0f485a56db7b61c2e4
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2014-10-23 11:01:27 +0300

    change controller scaffold template in order to build resource params

--- lib/generators/rails/templates/controller.rb
+++ lib/generators/rails/templates/controller.rb
@@ -2,4 +2,12 @@ class <%= controller_class_name %>Controller < InheritedResources::Base
 <% if options[:singleton] -%>
   defaults :singleton => true
 <% end -%>
+
+private
+
+  def <%= singular_name %>_params
+    params.fetch(:<%= singular_name %>, {}).permit(<%= attributes_names.map{ |a_name| ":#{a_name}" }.join(", ") %>)
+  end
+
 end
+
