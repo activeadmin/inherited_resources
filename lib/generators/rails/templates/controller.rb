72d826c6b4884460b6dc03ff6e7041a1f9c2137a
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2014-10-27 18:01:16 +0200

    generate resource params if rails 4 app or used strong params gem

--- lib/generators/rails/templates/controller.rb
+++ lib/generators/rails/templates/controller.rb
@@ -4,10 +4,10 @@ class <%= controller_class_name %>Controller < InheritedResources::Base
 <% end -%>
 
 private
-
-  def <%= singular_name %>_params
-    params.require(:<%= singular_name %>).permit(<%= attributes_names.map{ |a_name| ":#{a_name}" }.join(", ") %>)
-  end
-
+  <% if Rails::VERSION::MAJOR >= 4 || defined?(ActiveModel::ForbiddenAttributesProtection) %>
+    def <%= singular_name %>_params
+      params.require(:<%= singular_name %>).permit(<%= attributes_names.map{ |a_name| ":#{a_name}" }.join(", ") %>)
+    end
+  <% end %>
 end
 
