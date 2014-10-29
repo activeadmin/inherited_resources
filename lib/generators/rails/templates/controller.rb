f1be51bfae1524068d9f49ec7b2b741207ee5653
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-10-29 20:07:06 -0200

    Do not generate private keyword when it is not needed

--- lib/generators/rails/templates/controller.rb
+++ lib/generators/rails/templates/controller.rb
@@ -2,12 +2,13 @@ class <%= controller_class_name %>Controller < InheritedResources::Base
 <% if options[:singleton] -%>
   defaults :singleton => true
 <% end -%>
+<% if Rails::VERSION::MAJOR >= 4 || defined?(ActiveModel::ForbiddenAttributesProtection) -%>
 
-private
-  <% if Rails::VERSION::MAJOR >= 4 || defined?(ActiveModel::ForbiddenAttributesProtection) %>
+  private
+
     def <%= singular_name %>_params
       params.require(:<%= singular_name %>).permit(<%= attributes_names.map{ |a_name| ":#{a_name}" }.join(", ") %>)
     end
-  <% end %>
+<% end -%>
 end
 
