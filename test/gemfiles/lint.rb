ca610ccca4084edbacc0cc2edafb97a6273b88f7
David Rodríguez <deivid.rodriguez@riseup.net>
2019-03-14 10:28:33 +0100

    Check for outdated gemfiles

---
+++ test/gemfiles/lint.rb
@@ -0,0 +1,17 @@
+require "minitest"
+
+class GemfilesTest < Minitest::Test
+  [*Dir.glob("test/gemfiles/Gemfile-Rails-[0-9]-[0-9]"), "Gemfile"].each do |gemfile|
+    define_method :"test_#{gemfile}_is_up_to_date" do
+      current_lockfile = File.read("#{gemfile}.lock")
+
+      new_lockfile = Bundler.with_original_env do
+        `BUNDLE_GEMFILE=#{gemfile} bundle lock --print`
+      end
+
+      msg = "Please update #{gemfile}'s lock file with `BUNDLE_GEMFILE=#{gemfile} bundle install` and commit the result"
+
+      assert_equal current_lockfile, new_lockfile, msg
+    end
+  end
+end
