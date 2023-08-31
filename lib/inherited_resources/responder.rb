c0f0951de3473909279adaf8e462e11091538c98
Alexander Nickol <a.nickol@trust-nickol.de>
2023-08-31 08:31:01 +0200

    change default error_status & redirect_status codes
    update responder class to the current defaults for responder gem

--- lib/inherited_resources/responder.rb
+++ lib/inherited_resources/responder.rb
@@ -1,5 +1,10 @@
 module InheritedResources
   class Responder < ActionController::Responder
     include Responders::FlashResponder
+    include Responders::HttpCacheResponder
+  
+    # Configure default status codes for responding to errors and redirects.
+    self.error_status = :unprocessable_entity
+    self.redirect_status = :see_other
   end
 end
