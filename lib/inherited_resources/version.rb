d31573fc8c9bff34d548b379622e9bee88b9e50e
Geremia Taglialatela <tagliala@users.noreply.github.com>
2024-08-26 22:10:26 +0200

    Add frozen string literal magic comment (potential breaking change) (#933)
    Add frozen string literal magic comment
    
    This cop was disabled probably by mistake, since it is present in both
    Arbre and Active Admin gems
    
    The only production code affected is:
    - app/controllers/inherited_resources/base.rb
    - lib/inherited_resources/version.rb
    
    Change to `version.rb` is safe.
    
    After inspecting the source code of `InheritedResources` module, the
    change seems safe, but it can represent a breaking change in third-party application.
    
    Since we are bumping the major version number, it would be fine to break
    if documented.
    
    Fixed with `rubocop -A`
    
    Also adds `Style/Encoding` cop for parity with Arbre

--- lib/inherited_resources/version.rb
+++ lib/inherited_resources/version.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module InheritedResources
   VERSION = '2.0.0'.freeze
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 if ENV.fetch('COVERAGE', false)
   require 'simplecov'
   require 'simplecov-cobertura'
